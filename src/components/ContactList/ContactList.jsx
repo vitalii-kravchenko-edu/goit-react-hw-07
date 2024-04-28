c461d56d7ef83a1958030131042f0beadb2dbabe
vitalii-kravchenko-edu <opabbk@gmail.com>
2024-04-28 14:26:01 +0300

    fix contact filter

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,13 @@
 import { useSelector } from "react-redux";
-import { selectContacts } from "/src/redux/contactsSlice.js";
-import { selectNameFilter } from "../../redux/filtersSlice";
 
 import Contact from "../Contact/Contact";
+
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 import styles from "./ContactList.module.css";
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectNameFilter);
-  const filteredContacs = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const filteredContacs = useSelector(selectFilteredContacts);
 
   return (
     <ul className={styles.list}>
